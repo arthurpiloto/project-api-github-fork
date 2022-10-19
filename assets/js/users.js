e13720ff3465ebf8178e73720981cffe5a6fa7e0
Vinicius Santos <vo70346@gmail.com>
2022-10-19 11:12:20 -0300

    alterando nome do arquivo

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { getUsers } from "../js/fetchUsers.js"
+import { getUsers } from "./fetchUsers.js"
 import { createDiv, createImg, createSpan } from "./utils/createElements.js"
 // var username = document.getElementById('username').value
 // var infoData = await getInfos(username) // Essa linha dá o erro *** VM762:1 Uncaught SyntaxError: Unexpected token '<', "<!DOCTYPE "... is not valid JSON ***
@@ -10,22 +10,31 @@ import { createDiv, createImg, createSpan } from "./utils/createElements.js"
 
 
 let pageNumber = 1
+
 const createCards =  async () => {
     let username = document.getElementById('username').value
     var infoData = await getUsers(username, pageNumber)
 
     infoData.items.forEach(element => {
-        const container = document.querySelector('main')
+        const container = document.querySelector('.content-container')
         const card = createDiv('user-card')
         const usernameText = createSpan('user-name', element.login)
+        const userIcon = createImg('user-icon', element.avatar_url)
 
+        card.appendChild(userIcon)
         card.appendChild(usernameText)
         container.appendChild(card)
     });
 }
 
+const clearCards = () => {
+    const cards = document.querySelectorAll('.user-card')
+    cards.forEach((card) => card.remove())
+}
+
 const handleKeyPress = (event) => {
     if(event.key == 'Enter') {
+        clearCards()
         createCards()
     }
 }
@@ -36,6 +45,7 @@ document.getElementById('username')
 document.getElementById('button')
         .addEventListener('click', () => {
             pageNumber++
+            clearCards()
             createCards()
             console.log(pageNumber);
         })
