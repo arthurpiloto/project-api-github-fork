55103a0d50f52a3508d93cec3f0331c3d78c2da8
Vinicius Santos <vo70346@gmail.com>
2022-10-20 11:27:20 -0300

    novos arquivos

---
+++ assets/js/fetchUserInfo.js
@@ -0,0 +1,19 @@
+const getUserInfo = async (username) => {
+    const user = username
+
+    const url = `http://api.github.com/users/${user}`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+}
+
+const getUserInfoRepos = async (username) => {
+    const user = username
+
+    const url = `http://api.github.com/users/${user}/repos`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+}
