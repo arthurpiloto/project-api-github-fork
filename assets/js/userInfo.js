ca87f40799ed8693221517fa4e736dec090ca8db
Vinicius Santos <vo70346@gmail.com>
2022-10-25 07:35:24 -0300

    função para mostrar repositórios

--- assets/js/userInfo.js
+++ assets/js/userInfo.js
@@ -2,10 +2,30 @@
 
 const user = localStorage.getItem('user')
 
-const createRepos = () => {
+// Import das funções
+import { getUserInfo, getUserInfoRepos } from "./fetchUserInfo.js"
+import { createDiv, createSpan, createImg } from "./utils/createElements.js"
 
+const userInfoRepos = await getUserInfoRepos(user)
+const userInfo = await getUserInfo(user)
+
+const createRepos = (json) => {
+    const contentContainer = document.querySelector('.content-container')
+    const reposContainer = document.querySelector('.repos-container')
+
+    json.forEach(element => {
+        const reposName = createSpan('repost-name', `Repositório: ${element.name}`)
+        const reposLink = createSpan('repos-link', `Link do repositório: ${element.html_url}`)
+        reposContainer.appendChild(reposName)
+        reposContainer.appendChild(reposLink)
+        contentContainer.appendChild(reposContainer)
+    });
 }
 
-const createProfile = () => {
+createRepos(userInfoRepos)
+
+const createProfile = (json) => {
     
 }
+
+// createProfile(userInfo)
